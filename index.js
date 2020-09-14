b260028003487e77eb2aa780b13c9a83703469c0
antony_sanders <antony.devon92@gmail.com>
2020-09-14 02:30:03 -0400

    Done.

--- index.js
+++ index.js
@@ -1,27 +1,45 @@
 const fi = (function() {
-  return {
-    libraryMethod: function() {
-      return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
-    },
+    return {
+        libraryMethod: function() {
+            return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
+        },
 
-    each: function() {
+        each: function(collection, func) {
+            let newArr = Object.values(collection);
+            newArr.forEach(e => func(e))
+            return collection;
+        },
 
-    },
+        map: function(collection, func) {
+            let newArr = Object.values(collection);
+            let finalArr = [];
+            newArr.forEach(e => finalArr.push(func(e)));
+            return finalArr;
+        },
 
-    map: function() {
+        reduce: function(collection, func, acc) {
+            let start = !!acc ? acc : collection[0];
+            let newArr = start === acc ? collection : collection.slice(1);
+            newArr.forEach(e => {
+                start = func(start, e)
+            })
+            return start;
+        },
 
-    },
+        find: function(collection, predicate) {
+            if (!(collection instanceof Array))
+                collection = Object.values(collection)
+            collection.forEach(e => {
+                predicate(e);
+            })
+        },
 
-    reduce: function() {
+        functions: function() {
 
-    },
-
-    functions: function() {
-
-    },
+        },
 
 
-  }
+    }
 })()
 
 fi.libraryMethod()
